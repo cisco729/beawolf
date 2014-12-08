b7f0f67a30a7d70cd42bf476a4218bbe42a61ef1
Ryan <Ryan.Franciscus@hotmail.com>
2014-12-08 02:15:09 -0500

    Refactored DB Classes. Addition of Struts 1.3 Addition of DB Connections Addition of Action Class

--- docView/WebContent/jsp/configuration.jsp
+++ docView/WebContent/jsp/configuration.jsp
@@ -1,12 +1,8 @@
-<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
-    pageEncoding="ISO-8859-1"%>
-<%@ taglib uri="http://java.sun.com/jsp/jstl/sql" prefix="sql" %>
-<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 5 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
-<link rel="stylesheet" href="../themes/base.css">
+<link rel="stylesheet" href="themes/base.css">
 <title>Configuration</title>
 </head>
 <body>
---
+++ docView/src/com/fmsolutions/database/base/AbstractDAO.java
@@ -0,0 +1,56 @@
+package com.fmsolutions.database.base;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
+
+public class AbstractDAO {
+
+	static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
+	static final String DB_URL = "jdbc:mysql://localhost/doc";
+	
+	//Credentials
+	static final String DB_USER = "appdoc";
+	static final String DB_PASS = "p@ssword1";
+	Connection conn = null;
+	Statement stmt = null;
+	public ResultSet result;
+	
+	private void getConnection(){
+		try{
+			Class.forName("com.mysql.jdbc.Driver");
+			conn = DriverManager.getConnection(DB_URL, DB_USER, DB_PASS);
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+		
+	}
+	
+	public ResultSet dbSelect(String selectStatement){
+		ResultSet result = null;
+		this.getConnection();
+		try{
+			stmt = conn.createStatement();
+			result = stmt.executeQuery(selectStatement);
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+		return result;
+		
+	}
+	
+	public void cleanUp(){
+		if(conn!=null){
+			try{
+				stmt.close();
+				conn.close();
+			}catch(Exception e){
+				e.printStackTrace();
+			}
+			
+		}
+	}
+	
+}
