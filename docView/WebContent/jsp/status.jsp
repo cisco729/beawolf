1acd2334c309e8fa9b488995fd0982a4a91c8bfb
tsmusser <terrymusser@hotmail.com>
2014-12-14 16:27:53 -0500

    Added status JSP

---
+++ docView/WebContent/jsp/status.jsp
@@ -0,0 +1,38 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<%@ taglib uri="http://struts.apache.org/tags-bean" prefix="bean" %>
+<%@ taglib uri="http://struts.apache.org/tags-html" prefix="html" %>
+<%@ taglib uri="http://struts.apache.org/tags-logic" prefix="logic" %>    
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+<title>Status</title>
+<link rel="stylesheet" href="themes/base.css">
+<script type="text/javascript">
+function status(){
+	document.forms[0].action.value;
+	document.forms[0].method.value = "status";
+	document.forms[0].submit();
+}
+
+</script>
+</head>
+<body style="background-color: white;">
+	<%session.setAttribute("headerTitle","Status"); %>
+	<jsp:include page="components/header.jsp"></jsp:include>
+
+	<form name="statusForm" action="statusPage;">
+	<div class="StatusInfo">
+			Status Info:
+			<ul>
+				<li>
+					<span>Status Code:</span> <input type="text" name="statusCode"/>
+				</li>
+				<li>
+					<span>Status Name:</span>  <input type="text" name="statusName"/>
+				</li>
+			</ul>
+	</div>
+</body>
+</html>
--- docView/src/com/fmsolutions/folder/action/StatusAction.java
+++ docView/src/com/fmsolutions/folder/action/StatusAction.java
@@ -10,21 +10,37 @@ import org.apache.struts.action.ActionForward;
 import org.apache.struts.action.ActionMapping;
 import org.apache.struts.actions.DispatchAction;
 
+import com.fmsolutions.database.dao.LoginDao;
 import com.fmsolutions.database.dao.StatusDao;
+import com.fmsolutions.folder.login.LoginForm;
+import com.fmsolutions.folder.status.StatusForm;
 
 public class StatusAction extends DispatchAction{
 
 	public ActionForward statusPage(ActionMapping mapping, ActionForm form,
 			   HttpServletRequest request, HttpServletResponse response) throws Exception{
 
-	System.out.println("---Entered Status Action Class ---");
+		System.out.println("---Entered Status Action Class ---");
+		
+		StatusForm status = new StatusForm();
+		status = (StatusForm) form;
+		String forward = "statusPage";
 	
-	return (mapping.findForward("statusPage"));
+		status = (StatusForm) form;
+		
+		ArrayList<StatusInfo> listOfStatuses = this.getStatusData(response);
+		
+		// set values
+		status.setStatusCode(listOfStatuses.get(0).getStatusCode());
+		status.setStatusName(listOfStatuses.get(0).getStatusName());
+		status.setMaintId(listOfStatuses.get(0).getMaintId());
+		status.setMaintDate(listOfStatuses.get(0).getMaintDate());
+		
+		return mapping.findForward(forward);
 	
 	}
 	
-	public void getStatusData(ActionMapping mapping, ActionForm form,
-		HttpServletRequest request, HttpServletResponse response) throws Exception{
+	public ArrayList<StatusInfo> getStatusData(HttpServletResponse response) throws Exception{
 		System.out.println("---Entered Status Action Class ---");
 		
 		StatusDao status = new StatusDao();
@@ -38,6 +54,8 @@ public class StatusAction extends DispatchAction{
 		response.getWriter().print(listOfStatuses.get(0).getMaintId());
 		response.getWriter().print(listOfStatuses.get(0).getMaintDate());
 		response.flushBuffer();
+		
+		return listOfStatuses;
 	}
 	
 }
